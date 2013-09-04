32ff6066926e85410916ed7198e15c9ed131e8d7
Dave Shanley <dave@crowdcompass.com>
2013-09-03 21:40:34 -0700

    Added Sproutworx as admin user

--- just_arrived.js
+++ just_arrived.js
@@ -3,7 +3,7 @@ Attendees = new Meteor.Collection("attendees");
 EMAIL_REGEX = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 // Github account usernames of admin users
-var ADMIN_USERS = ['dshanley'];
+var ADMIN_USERS = ['dshanley', 'Sproutworx'];
 function isAdmin(userId) {
   var user = Meteor.users.findOne({_id: userId});
   try {
